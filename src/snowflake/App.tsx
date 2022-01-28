69d46bf3c6283ae8248be591fc9f12ba1a86e463
David Wisner <dwisner6@gmail.com>
2022-01-28 12:13:14 +0800

    update build

---
+++ src/snowflake/App.tsx
@@ -0,0 +1,86 @@
+import Loader from 'components/Loader'
+import ApeModeQueryParamReader from 'hooks/useApeModeQueryParamReader'
+import { lazy, Suspense } from 'react'
+import { Redirect, Route, Switch } from 'react-router-dom'
+import styled from 'styled-components/macro'
+import AddressClaimModal from '../components/claim/AddressClaimModal'
+import ErrorBoundary from '../components/ErrorBoundary'
+import Header from '../components/Header'
+import Polling from '../components/Header/Polling'
+import Popups from '../components/Popups'
+import Web3ReactManager from '../components/Web3ReactManager'
+import { useModalOpen, useToggleModal } from '../state/application/hooks'
+import { ApplicationModal } from '../state/application/reducer'
+import DarkModeQueryParamReader from '../theme/DarkModeQueryParamReader'
+import Swap from '../pages/Swap'
+import { OpenClaimAddressModalAndRedirectToSwap, RedirectPathToSwapOnly, RedirectToSwap } from '../pages/Swap/redirects'
+
+// const Vote = lazy(() => import('./Vote'))
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: flex-start;
+`
+
+const BodyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 120px 16px 0px 16px;
+  align-items: center;
+  flex: 1;
+  z-index: 1;
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    padding: 4rem 8px 16px 8px;
+  `};
+`
+
+const HeaderWrapper = styled.div`
+  ${({ theme }) => theme.flexRowNoWrap}
+  width: 100%;
+  justify-content: space-between;
+  position: fixed;
+  top: 0;
+  z-index: 2;
+`
+
+const Marginer = styled.div`
+  margin-top: 5rem;
+`
+
+function TopLevelModals() {
+  const open = useModalOpen(ApplicationModal.ADDRESS_CLAIM)
+  const toggle = useToggleModal(ApplicationModal.ADDRESS_CLAIM)
+  return <AddressClaimModal isOpen={open} onDismiss={toggle} />
+}
+
+export default function App() {
+  return (
+    <ErrorBoundary>
+      <Route component={DarkModeQueryParamReader} />
+      <Route component={ApeModeQueryParamReader} />
+      <Web3ReactManager>
+        <AppWrapper>
+          <HeaderWrapper>
+            <Header />
+          </HeaderWrapper>
+          <BodyWrapper>
+            <Popups />
+            <Polling />
+            <TopLevelModals />
+            <Suspense fallback={<Loader />}>
+              <Switch>
+                <Route exact strict path="/swap/:outputCurrency" component={RedirectToSwap} />
+                <Route exact strict path="/swap" component={Swap} />
+                <Route component={RedirectPathToSwapOnly} />
+              </Switch>
+            </Suspense>
+            <Marginer />
+          </BodyWrapper>
+        </AppWrapper>
+      </Web3ReactManager>
+    </ErrorBoundary>
+  )
+}
--- src/snowflake/index.ts
+++ src/snowflake/index.ts
@@ -1,5 +1,6 @@
 // snowflake exports
 
+export { default as App } from './App'
 export { default as Swap } from '../pages/Swap'
 export * as serviceWorkerRegistration from '../serviceWorkerRegistration'
 export * from './Providers'
